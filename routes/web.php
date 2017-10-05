60a41d004565477fb39270da95dbcc8d81428168
mayooshmp <mayooshikm@gmail.com>
2017-10-05 12:56:49 +0530

    3

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,33 @@ Route::post('/task','TasksController@create');
 
 Route::get('/task/{task}','TasksController@edit');
 Route::post('/task/{task}','TasksController@update');
+
+Route::any('captcha-test', function()
+    {
+        if (Request::getMethod() == 'POST')
+        {
+            $rules = ['captcha' => 'required|captcha'];
+            $validator = Validator::make(Input::all(), $rules);
+            if ($validator->fails())
+            {
+                echo '<p style="color: #ff0000;">Incorrect!</p>';
+            }
+            else
+            {
+                echo '<p style="color: #00ff30;">Matched :)</p>';
+            }
+        }
+    
+        $form = '<form method="post" action="captcha-test">';
+        $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+        $form .= '<p>' . captcha_img() . '</p>';
+        $form .= '<p><input type="text" name="captcha"></p>';
+        $form .= '<p><button type="submit" name="check">Check</button></p>';
+        $form .= '</form>';
+        return $form;
+    });
+
+
 /*Route::get('/', function () {
     return view('welcome');
 });
